0b2a4e31d21b52c8c86e7b7d38d372490865b8c2
Phallyn <phabercoss@hawk.iit.edu>
2019-10-02 13:40:12 -0500

    Added padding font-size and javascript button for submit

---
+++ script.js
@@ -0,0 +1,6 @@
+function init() {
+var click = document.getElementById("submit");
+click.addEventListener("click", function()
+{ alert("Thank you for signing up for our newsletter!") });
+}
+window.addEventListener('load', init);
