d2ba53cf6267092186ee7cf845c98be98a66dd61
Phallyn <phabercoss@hawk.iit.edu>
2019-10-30 22:00:38 -0500

    Added new javascript and link for modular scale

--- js/script.js
+++ js/script.js
@@ -1,5 +1,34 @@
-function init() {
-var click=document.getElementById("submit");
-  click.addEventListener("click", function() { alert("Thank you for signing up for our newsletter!") });
-}
-window.addEventListener('load', init);
+/* when filling in an invalid letter or word in email form and clicking submit, it will show up red and say to enter a valid email address*/ 
+(function() {
+  var submit_button = document.querySelector('#submit');
+  var newsletter = document.querySelector('#form');
+  var email_input = document.querySelector('#user_email');
+
+  function equal(value, condition) {
+    return value === condition;
+  }
+
+  function validate(value, check, condition) {
+    if (equal(typeof(check.test), 'function')) {
+      return check.test(value);
+    }
+    else if (equal(typeof(check), 'function')) {
+      return check(value, condition);
+    }
+    else {
+      return false;
+    }
+  }
+
+  function check_email(value) {
+    var email = value;
+    return validate(email, /^[^@\s]+@[^@\s]+$/g);
+  }
+
+  newsletter.addEventListener('keyup', function() {
+    var email_value = email_input.value;
+    if (check_email(email_value)) {
+      submit_button.removeAttribute('disabled');
+    }
+  });
+}());
--- js/validate.js
+++
@@ -1,6 +0,0 @@
-function emailValidate() {
-if (document.getElementById("email").value == "")
-{
-  alert("Please enter your email.");
-  return false;
-}
